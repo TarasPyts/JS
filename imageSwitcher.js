2d45ee4affd362db857d8a7134cc710af376b165
TarasPyts <taras.a.pyts@gmail.com>
2022-12-26 12:59:41 +0200

    switcher

---
+++ imageSwitcher.js
@@ -0,0 +1,22 @@
+let i = 0;
+
+   let    images = ["images/img1.jpg",
+                    "images/img2.jpg",
+                    "images/img3.jpg",
+                    "images/img4.jpg",
+                    "images/img5.jpg"];
+
+function mySlide(param)
+{
+  if (param === 'next')
+  {
+    i++;
+    if(i === images.length) { i = 0 }
+  }else{
+    i--;
+    if(i < 0) { i = images.length -1 }
+    }
+  document.getElementById('slide').src = images[i];
+  }
+
+
